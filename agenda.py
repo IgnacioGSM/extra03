3f156f13fa4f9aea4a6e7b58f03f903c372fb2a4
Ignacio <iglausser2024@alu.uct.cl>
2024-05-13 23:13:18 -0400

    Función para elegir una opcion del menú

--- agenda.py
+++ agenda.py
@@ -2,3 +2,11 @@ import pandas as pd
 import textual as te
 import numpy as np
 
+def seleccion(x, mensaje="Texto de ejemplo"):
+    while True:
+        try: elegido = int(input(mensaje))
+        except ValueError: print("Error, escriba un número en las opciones de arriba.")
+        else:
+            if elegido in range(1,x+1):
+                return elegido
+            else: print("No hay una opción con ese número, intente otro.")
