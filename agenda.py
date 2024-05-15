dba7ce6a03831e2249b92c294877c8c08c731a3d
Ignacio G. <162763971+IgnacioGSM@users.noreply.github.com>
2024-05-15 14:45:32 -0400

    Funcion de entrada alfanumerica

--- agenda.py
+++ agenda.py
@@ -2,7 +2,11 @@
 #import textual as te
 #import numpy as np
 
-def entrada_seleccion_int(x, mensaje="Texto de ejemplo"):
+
+#-----------------------------------------------------------------------------------------
+# FUNCIONES DE ENTRADA
+#-----------------------------------------------------------------------------------------
+def entrada_seleccion_int(x, mensaje="Elige una opcion: "):
     while True:
         try: entrada = int(input(mensaje))
         except ValueError: print("Error, escriba un número en las opciones de arriba.")
@@ -11,7 +15,7 @@ def entrada_seleccion_int(x, mensaje="Texto de ejemplo"):
                 return entrada
             else: print("No hay una opción con ese número, intente otro.")
 
-def entrada_alfa(mensaje="Texto de ejemplo 2"):
+def entrada_alfa(mensaje="Ingresar palabras: "):
     while True:
         entrada = input(mensaje)
         entrada = entrada.split()
@@ -31,9 +35,27 @@ def entrada_rut(mensaje="Ingresar rut sin guiones ni puntos: "):
             if entrada[0:-1].isnumeric() and (entrada[-1].isnumeric() or entrada[-1] == "K"):
                 return entrada[0:-1] + "-" + entrada[-1]
             else:
-                print("Rut invalido, asegurese de haberlo escrito bien")
+                print("Rut invalido, asegurese de haber escrito bien el rut")
         else:
-                print("Rut invalido, asegurese de haberlo escrito bien")
+                print("Rut invalido, asegurese de haber escrito bien el rut")
 
-mirut = entrada_rut()
-print(mirut)
+def entrada_numero(mensaje="Ingrese un numero: "):
+    while True:
+        try: entrada = int(input(mensaje))
+        except ValueError: print("Error, asegurese de ingresar solo numeros enteros")
+        else: return entrada
+
+def entrada_alfanumerico(mensaje="Ingresa algo: "):
+    while True:
+        entrada = input(mensaje)
+        entrada = entrada.split()
+        alfanum = True
+        for x in entrada:
+            if not x.isalnum():
+                alfanum = False
+                print("Error, Evite usar guiones, comas, puntos u otros simbolos")
+                break
+        if alfanum: return " ".join(entrada)
+#-----------------------------------------------------------------------------------------
+# FUNCIONES DE ENTRADA
+#-----------------------------------------------------------------------------------------
