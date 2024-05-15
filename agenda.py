3997367c038db75150487d4ba130d96cd481bcf7
Ignacio G. <162763971+IgnacioGSM@users.noreply.github.com>
2024-05-14 21:23:33 -0400

    Añadida función que solo acepta espacios y letras

--- agenda.py
+++ agenda.py
@@ -1,12 +1,29 @@
-import pandas as pd
-import textual as te
-import numpy as np
+#import pandas as pd
+#import textual as te
+#import numpy as np
 
-def seleccion(x, mensaje="Texto de ejemplo"):
+def entrada_int(x, mensaje="Texto de ejemplo"):
     while True:
-        try: elegido = int(input(mensaje))
+        try: entrada = int(input(mensaje))
         except ValueError: print("Error, escriba un número en las opciones de arriba.")
         else:
-            if elegido in range(1,x+1):
-                return elegido
+            if entrada in range(1,x+1):
+                return entrada
             else: print("No hay una opción con ese número, intente otro.")
+
+def entrada_alfa(mensaje="Texto de ejemplo 2"):
+    while True:
+        entrada = input(mensaje)
+        entrada = entrada.split()
+        alfa = True
+        for x in entrada:
+            if not x.isalpha():
+                alfa = False
+                print("Error, asegurese de solo escribir letras")
+                break
+        if alfa == True: return " ".join(entrada)
+            
+        
+
+hola = entrada_alfa("auuuu: ")
+print(hola)
